b1c46ef82f3580d55b2f10b8ab4e55694ac72a3a
Carter Tsai <hamming1@gmail.com>
2014-04-04 22:29:37 +0800

    Add Test, Update package.json and Fixed typo

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-var base = require('./base');
-var api = require('./api');
+var passport = require("passport")
+    , base = require('./base')
+    , api = require('./api');
 
 exports.path = function(app){
     app.get('/', base.index);
     app.post('/api/member/login', 
-                passport.authenticate("local"}),
+                passport.authenticate("local"),
                 api.member.login);
     app.post('/api/member/logout', api.member.logout); 
 };
-
-
